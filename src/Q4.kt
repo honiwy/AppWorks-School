338cb8cc5ba4432509e348a45ab85a459336f204
WYC <honiwy@gmail.com>
2019-11-10 09:20:49 -0500

    Add Q5 of week 1 assignment

--- src/Q4.kt
+++ src/Q4.kt
@@ -3,12 +3,25 @@
 //b. Add a method, attack(), that print "<the name passed in> use Fist Attack!".
 //c. In the main() function, create an instance of Human, and call the attack() method to print
 fun main(args: Array<String>){
-val human=Human("Rachel")
+    val human=Human("Rachel")
     human.attack()
-
+    val mage=Mage("Mickey")
+    mage.attack()
 }
-class Human(val name: String){
-   fun attack(){
+open class Human(val name: String){
+   open fun attack(){
         println("$name use Fist Attack!")
     }
 }
+
+//5. Create a subclass of Human; name it Mage, includes:
+//a. Pass in a mandatory String argument for the name, also for the constructor of Human.
+//b. Override the attack() method to print "<the name passed in> use Fireball!".
+//c. In the main() function, create an instance of Mage, and call the attack() method to print.
+class Mage(val nameMage:String): Human(nameMage) {
+    override fun attack() {
+        println("$nameMage use Fireball!" )
+    }
+}
+
+
